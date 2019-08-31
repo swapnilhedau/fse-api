10ed6c9088e2306b1b586ee8266e4ec2f8d561fc
Swapnil Hedau <swapnil.hedau@cognizant.com>
2019-08-31 07:44:19 +0530

    first commit

---
+++ src/main/java/com/cts/fse/FseApiApplication.java
@@ -0,0 +1,13 @@
+package com.cts.fse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FseApiApplication.class, args);
+	}
+
+}
